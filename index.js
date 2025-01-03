781a46ae6faaef8abf6d4172c44c1b49389c9d5d
Sathishthanneeru <sathishthanneeru36@gmail.com>
2025-01-03 23:33:27 +0530

    new

---
+++ index.js
@@ -0,0 +1,125 @@
+// let x = 5;
+// while (x < 3) {
+//   console.log(x);
+//   x++;
+// }
+
+// // ans: NO output
+
+// let y = 0;
+// do {
+//   console.log(y);
+//   y++;
+// } while (y < 2);
+
+// // ans: 0,1
+
+// // 3rd question ans is "b"
+
+// let count = 5;
+// do {
+//   console.log(count);
+//   count--;
+// } while (count > 0);
+
+// // ans : 5,4,3,2,1
+
+// let z = 0;
+// while (z < 3) {
+//   console.log(z);
+//   z++;
+// }
+
+// // ans: 0,1,2
+
+// let i = 0;
+// while (i < 5) {
+//   console.log(i);
+//   if (i === 3) break;
+//   i++;
+// }
+
+// // ans:0,1,2,3
+
+// let a = 0;
+// do {
+//   console.log(a);
+//   a++;
+// } while (a < 0);
+
+
+// // ans: 0
+
+
+
+// let x=1;
+// while(x<=5){
+//     console.log(x);
+//     x++;
+// }
+
+
+// ans : 1,2,3,4,5
+
+// let x1=1;
+// do {
+//     console.log(x1);
+//     x1++;
+
+// }
+// while(x1<=5)
+
+// // ans : 1,2,3,4,5
+
+
+// let num=0;
+// while(num<10){
+//     console.log(num);
+//     if (num===3){
+//         console.log("exist the loop");
+//         break;
+//     }
+//     num++;
+// }
+
+
+
+// ans: 0,1,2,3 exist the loop
+
+
+
+// let num=1;
+// while(num<=10){
+//     if (num == 5){
+//         num++;
+//         continue;
+
+//     }
+//     console.log(num);
+//     num++;
+ 
+// }
+
+
+// ans:1,2,3,4,6,7,8,9,10     5 is missing
+
+
+
+
+// let x=0;
+
+// while(x<=20){
+//     if (x%2==0){
+//         console.log(x);
+
+//     }
+//     x++;
+// }
+
+
+// ans: 2,4,6,8,10,12,14,16,18,20   printing even numbers
+
+
+
+
+
